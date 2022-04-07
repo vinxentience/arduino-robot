12395d6379e7144f69fb2177a31243cd6bfc2dd8
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-04-07 19:50:53 +0800

    Added Constants file

---
+++ Constants.h
@@ -0,0 +1,20 @@
+const int PIN_DHT = A0;
+
+const int PIN_BUZZER = 7;
+const int PIN_TRIG = 8;
+const int PIN_SERVO = 9;
+const int PIN_ECHO = 10;
+
+const int PIN_LEFT_LINE_SENSOR = 5;
+const int PIN_RIGHT_LINE_SENSOR = 6;
+
+const int PIN_LEFT_RED = 2;
+const int PIN_LEFT_GREEN = 3;
+const int PIN_LEFT_BLUE = 4;
+
+const int PIN_RIGHT_RED = 13;
+const int PIN_RIGHT_GREEN = 12;
+const int PIN_RIGHT_BLUE = 11;
+
+const int PIN_LEFT_RGB[3] = {PIN_LEFT_RED, PIN_LEFT_GREEN, PIN_LEFT_BLUE};
+const int PIN_RIGHT_RGB[3] = {PIN_RIGHT_RED, PIN_RIGHT_GREEN, PIN_RIGHT_BLUE};
