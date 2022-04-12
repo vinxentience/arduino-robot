bf73e80b248569dca1fa1c7dd985d0905b94b467
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-04-12 14:31:34 +0800

    Add motor

--- FINAL_PROJECT.ino
+++ FINAL_PROJECT.ino
@@ -1,159 +1,187 @@
-#include <dht.h>
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
+#include "Constants.h"
+
 #include <Servo.h>
+#include <AFMotor.h>
+
+#define MAX_DISTANCE 200
+#define MAX_SPEED 100 // sets speed of DC  motors
+#define MAX_SPEED_OFFSET 20
+
+AF_DCMotor leftMotor(1);
+AF_DCMotor rightMotor(3);
+
+boolean goesForward = false;
+int distance = 100;
+int speedSet = 0;
+
 Servo servo;
 
-//DHT11
-const int dht_pin = A0;
-int degree = 0;
-//Buzzer
-const int buzzer = 7;
-
-//RGB LED
-const int red_1 = 2;
-const int green_1 = 3;
-const int blue_1 = 4;
-const int red_2 = 13;
-const int green_2 = 12;
-const int blue_2 = 11;
-const int lineSensor_1 = 5;
-const int lineSensor_2 = 6;
-const int servo_pin = 9;
-//Ultrasonic
 long duration;
-int distance;
-const int trigPin = 8;
-const int echoPin = 10;
+int ultraSonicDistance; // centimeter
 
-//Objects
-LiquidCrystal_I2C lcd(0x27, 16, 2);
-dht DHT;
+void setup()
+{
+    // put your setup code here, to run once:
+    Serial.begin(9600);
+    servo.attach(PIN_SERVO);
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  lcd.begin();
-  servo.attach(servo_pin);
-  
-  delay(2000);
-  pinMode(buzzer, OUTPUT);
-  pinMode(red_1, OUTPUT);
-  pinMode(green_1, OUTPUT);
-  pinMode(blue_1, OUTPUT);
-  pinMode(red_2, OUTPUT);
-  pinMode(green_2, OUTPUT);
-  pinMode(blue_2, OUTPUT);
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  pinMode(lineSensor_1, INPUT);
-  pinMode(lineSensor_2, INPUT);
+    pinMode(PIN_TRIG, OUTPUT);
+    pinMode(PIN_ECHO, INPUT);
+    delay(500);
+
+    leftMotor.setSpeed(MAX_SPEED);
+    rightMotor.setSpeed(MAX_SPEED);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  dht();
+void loop()
+{
 
-//  for(degree = 0; degree <= 180; degree += 1){
-//    servo.write(degree);
-//    delay(10);
-//  }
-  
-  //ULTRASONIC
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
+    int distanceLeft = 0;
+    int distanceRight = 0;
+    delay(40);
 
-  duration = pulseIn(echoPin, HIGH);
+    ultraSonicDistance = lookForward();
 
-  distance = duration * 0.034 / 2;
+    if (ultraSonicDistance <= 15)
+    {
 
-  Serial.println(digitalRead(lineSensor_1));
-  Serial.println(digitalRead(lineSensor_2));
-  
-  changeLightFromDistance(distance);
+        moveStop();
+        delay(100);
+        moveBackward();
+        delay(300);
+        moveStop();
+        delay(200);
 
-  lcd.clear();
-}
+        playBuzzer();
 
-void setColor(int redVal, int greenVal, int blueVal){
-  analogWrite(red_1, redVal);
-  analogWrite(green_1, greenVal);
-  analogWrite(blue_1, blueVal);
-}
-void setColor2(int redVal, int greenVal, int blueVal){
-  analogWrite(red_2, redVal);
-  analogWrite(green_2, greenVal);
-  analogWrite(blue_2, blueVal);
-}
+        // Get distance from left and right
+        distanceRight = lookRight();
+        delay(300);
+        distanceLeft = lookLeft();
+        delay(300);
 
-void lightBlue() {
-  setColor(0, 0, 255);
-  setColor2(0, 0, 255);
-}
-
-void lightRed() {
-  setColor(255, 0,0);
-  setColor2(255, 0,0);
-}
-
-void lightGreen() {
-  setColor(0, 255, 0);
-  setColor2(0, 255, 0);
-}
-
-void biot(){
-  lightRed();
-  delay(300);
-  lightGreen();
-  delay(300);
-  lightBlue();
-  delay(300);
-}
-
-void dht(){
-  //DHT
-  DHT.read11(dht_pin);
-  double temp = DHT.temperature;
-  if(temp > 33){
-    lcd.setCursor(0,1);
-    lcd.print("Hot temp");
-    delay(300);
-  }else if(temp < 29){
-    lcd.setCursor(0,0);
-    lcd.print("Cold temp");
-    delay(300);
-  }else{
-    lcd.setCursor(0,0);
-    lcd.print("Normal temp");
-  }
-}
-
-void changeLightFromDistance(int distance) {
-  
-    if (distance < 15) { 
-      lightRed();
-      tone(buzzer, 700, 1000);
-      lcd.setCursor(0,1);
-      lcd.print("Obstacle found");
-      checkDirection();
-      delay(1000);
-    } else {
-      biot();
-      lcd.setCursor(0,1);
-      lcd.print("");
-      noTone(buzzer);
-      delay(500);
+        if (distanceRight >= distanceLeft)
+        {
+            turnRight();
+            moveStop();
+        }
+        else
+        {
+            turnLeft();
+            moveStop();
+        }
+    }
+    else
+    {
+        moveForward();
     }
 }
 
-void checkDirection(){
-  servo.write(-180);
-  delay(700);
-  servo.write(180);
-  delay(700);
-  servo.write(90);
+void playBuzzer()
+{
+    tone(PIN_BUZZER, 1500, 300);
+    delay(500);
+    // tone(PIN_BUZZER, 500, 300);
+    // delay(500);
+    // tone(PIN_BUZZER, 1500, 300);
+    // delay(300);
+}
+
+int readDistance()
+{
+    // Clears the trigPin condition
+    digitalWrite(PIN_TRIG, LOW);
+    delayMicroseconds(2);
+    // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+    digitalWrite(PIN_TRIG, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(PIN_TRIG, LOW);
+    // Reads the echoPin, returns the sound wave travel time in microseconds
+    int duration = pulseIn(PIN_ECHO, HIGH);
+
+    return duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+}
+
+int lookForward()
+{
+    servo.write(90);
+    int value = readDistance();
+    readDistance();
+
+    return value;
+}
+
+int lookLeft()
+{
+    servo.write(180);
+    int value = readDistance();
+    readDistance();
+    servo.write(90);
+
+    return value;
+}
+
+int lookRight()
+{
+    servo.write(0);
+
+    int value = readDistance();
+    readDistance();
+    servo.write(90);
+
+    return value;
+}
+
+void moveStop()
+{
+
+    Serial.println("Move Stop");
+
+    leftMotor.run(RELEASE);
+    rightMotor.run(RELEASE);
+}
+
+void moveForward()
+{
+    Serial.println("Move Forward");
+
+    if (!goesForward)
+    {
+        goesForward = true;
+
+        leftMotor.run(FORWARD);
+        rightMotor.run(FORWARD);
+    }
+}
+
+void moveBackward()
+{
+    Serial.println("Move Backward");
+
+    goesForward = false;
+
+    leftMotor.run(BACKWARD);
+    rightMotor.run(BACKWARD);
+}
+
+void turnRight()
+{
+    Serial.println("Turn Right");
+
+    leftMotor.run(FORWARD);
+    rightMotor.run(BACKWARD);
+    delay(500);
+    leftMotor.run(FORWARD);
+    rightMotor.run(FORWARD);
+}
+
+void turnLeft()
+{
+    Serial.println("Turn left");
+
+    leftMotor.run(BACKWARD);
+    rightMotor.run(FORWARD);
+    delay(500);
+    leftMotor.run(FORWARD);
+    rightMotor.run(FORWARD);
 }
